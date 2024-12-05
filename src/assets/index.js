f3def57b5759e08fd4d48b67a0ccf83bd85f93a6
Yonas Enchalew <yonasenchalew@gmail.com>
2024-12-05 11:20:56 +0300

    yon_portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,98 @@
+/*-------------------images-------------------*/
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+/*-------------------tech icons-------------------*/
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import vue from "./tech/vuejs.svg";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+/*-------------------company images-------------------*/
+import meta from "./company/meta.png";
+import getnet from "./company/getnet.png";
+import aastu from "./company/AASTU.jpg";
+import minabIcon from "./minabicon.png";
+import a2sv from "./company/a2svlogo.jpeg";
+
+/*-------------------project images-------------------*/
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import shemach from "./shemach.png";
+import recipe from "./recipe.png";
+import recipe2 from "./recipe2.png";
+import kena from "./kena.png";
+import sheger from "./sheger.png";
+import plan from "./plan.png";
+import splitbor from "./split.png";
+import tarixchi from "./tartxchi.png";
+
+/*-------------------social icons-------------------*/
+import instagram from "./instagram.png";
+import linkedin from "./linkedin.png";
+import telegram from "./telegram.png";
+import twitter from "./twitter.png";
+import yonaspic from "./photo_of_jann.jpg";
+
+export {
+	splitbor,
+	tarixchi,
+	yonaspic,
+	logo,
+	backend,
+	creator,
+	mobile,
+	web,
+	github,
+	menu,
+	close,
+	css,
+	docker,
+	vue,
+	figma,
+	git,
+	html,
+	javascript,
+	mongodb,
+	nodejs,
+	reactjs,
+	redux,
+	tailwind,
+	typescript,
+	threejs,
+	meta,
+	getnet,
+	aastu,
+	carrent,
+	jobit,
+	tripguide,
+	instagram,
+	linkedin,
+	telegram,
+	twitter,
+	shemach,
+	recipe,
+	recipe2,
+	kena,
+	sheger,
+	plan,
+	minabIcon,
+	a2sv,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,474 @@
+import {
+	mobile,
+	backend,
+	creator,
+	web,
+	javascript,
+	typescript,
+	html,
+	css,
+	reactjs,
+	vue,
+	redux,
+	tailwind,
+	nodejs,
+	mongodb,
+	git,
+	figma,
+	docker,
+	meta,
+	getnet,
+	aastu,
+	threejs,
+	github,
+	linkedin,
+	telegram,
+	twitter,
+	instagram,
+	shemach,
+	recipe,
+	recipe2,
+	minabIcon,
+	kena,
+	sheger,
+	plan,
+	a2sv,
+	splitbor,
+	tarixchi,
+} from "../assets";
+
+export const socialLinks = [
+	{
+		id: "github",
+		icon: github,
+		link: "https://github.com/yonas1219",
+		bg: "#181717",
+	},
+	{
+		id: "linkedin",
+		icon: linkedin,
+		link: "https://www.linkedin.com/in/yonas-enchalew-2a869822a/",
+		bg: "#0077B5",
+	},
+	{
+		id: "twitter",
+		icon: twitter,
+		link: "https://twitter.com/YonasYo1219",
+		bg: "#1DA1F2",
+	},
+	{
+		id: "instagram",
+		icon: instagram,
+		link: "https://www.instagram.com/jann_19_19/?hl=en",
+		bg: "#E1306C",
+	},
+	{
+		id: "telegram",
+		icon: telegram,
+		link: "https://t.me/yonas1993",
+		bg: "#0088CC",
+	},
+];
+
+export const navLinks = [
+	{
+		id: "about",
+		title: "About",
+	},
+	{
+		id: "work",
+		title: "Work",
+	},
+	{
+		id: "projects",
+		title: "Projects",
+	},
+];
+
+const services = [
+	{
+		title: "Web Developer",
+		icon: web,
+	},
+	{
+		title: "React  Developer",
+		icon: web,
+	},
+	{
+		title: "Backend Developer",
+		icon: backend,
+	},
+	{
+		title: "Vue  Developer",
+		icon: web,
+	},
+];
+
+const technologies = [
+	{
+		name: "HTML 5",
+		icon: html,
+	},
+	{
+		name: "CSS 3",
+		icon: css,
+	},
+	{
+		name: "JavaScript",
+		icon: javascript,
+	},
+	{
+		name: "TypeScript",
+		icon: typescript,
+	},
+	{
+		name: "React JS",
+		icon: reactjs,
+	},
+	{
+		name: "Vue js",
+		icon: vue,
+	},
+	{
+		name: "Redux Toolkit",
+		icon: redux,
+	},
+	{
+		name: "Tailwind CSS",
+		icon: tailwind,
+	},
+	{
+		name: "Node JS",
+		icon: nodejs,
+	},
+	{
+		name: "MongoDB",
+		icon: mongodb,
+	},
+	{
+		name: "Three JS",
+		icon: threejs,
+	},
+	{
+		name: "git",
+		icon: git,
+	},
+	{
+		name: "figma",
+		icon: figma,
+	},
+	{
+		name: "docker",
+		icon: docker,
+	},
+];
+
+const experiences = [
+	{
+		title: "Full Stack Developer",
+		company_name: "Dan Energy Ethiopia Research and Development",
+		website: "https://www.danenergyethiopia.com/",
+		icon: minabIcon,
+		iconBg: "#E6DEDD",
+		date: "Oct 2023 - present",
+		points: [
+			"Developing  web application using Vue.js Go and Hasura.",
+			"Collaborating with cross-functional teams ",
+			"Implementing responsive design and ensuring cross-browser compatibility using tailwind.",
+			"Creating enterprise level website with Hasura and Nuxt.js",
+		],
+	},
+
+	{
+		title: "Front end Developer intern",
+		company_name: "Get-net software solutions",
+		icon: getnet,
+		website: "https://www.getnetsoft.com/",
+		iconBg: "#E6DEDD",
+		date: "Jun 2023 - sept 2023",
+		points: [
+			"Developing  web application using React.js and other related technologies.",
+			"Collaborating with cross-functional teams ",
+			"Implementing responsive design and ensuring cross-browser compatibility.",
+			"Participating in code reviews and providing constructive feedback to other developers.",
+		],
+	},
+	{
+		title: "Data Structure and Algorithm Trainee ",
+		company_name: "A2SV",
+		icon: a2sv,
+		website: "https://a2sv.org/",
+		iconBg: "#E6DEDD",
+		date: "Dec 2022 - 2023 Sept",
+
+		points: [
+			"I devoted a full year to thorough, in-person study of data structures and algorithms.",
+			"solved more than 400  DSA questions on LeetCode and other platforms.",
+			"Developed teamwork skills and gained practical experience in collaborating effectively within a group.",
+		],
+	},
+	{
+		title: "Software Engineering student ",
+		company_name: "AASTU",
+		icon: aastu,
+		website: "http://www.aastu.edu.et/",
+		iconBg: "#E6DEDD",
+		date: "Sep 2019 - Present",
+		points: [
+			"Studied various programming languages and technology stacks.",
+			"Participated in a variety of projects, both collaboratively within a team and individually.",
+			"Acquired knowledge in diverse Software Development Life Cycle (SDLC) methodologies and gained  documenting project requirements.",
+			"Developed teamwork skills and gained practical experience in collaborating effectively within a group.",
+		],
+	},
+];
+
+const projects = [
+	{
+		name: "Kena",
+		description:
+			"Kena, under EYEA, fuels youth entrepreneurship through training, mentorship, and financial support. Its innovation competitions and strategic partnerships enrich a dynamic ecosystem, propelling the success of youth-led businesses.",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Nuxt.js",
+				color: "green-text-gradient",
+			},
+			{
+				name: "Vue Apollo",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "graphql",
+				color: "pink-text-gradient",
+			},
+
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+		],
+		image: kena,
+		source_code_link: "",
+		web_link: "https://kena.eyea.et/",
+	},
+	{
+		name: "Sheger Gebeta",
+		description:
+			"Indulge in culinary exploration! Discover and share delectable recipes on this intuitive, user-friendly platform designed for passionate food enthusiasts. Join the flavorful journey!",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Nuxt.js",
+				color: "green-text-gradient",
+			},
+			{
+				name: "Vue Apollo",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "graphql",
+				color: "pink-text-gradient",
+			},
+
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+		],
+		image: sheger,
+		source_code_link: "",
+		web_link: "https://preview.shegergebeta.com/",
+	},
+	{
+		name: "Plan international",
+		description:
+			"The goal of the project is to empower working and stay-at-home moms through establishing home-based childcare services in their own homes. In doing so, they will have their own business in a sector that is in high demand and continues to do so.",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Nuxt.js",
+				color: "green-text-gradient",
+			},
+			{
+				name: "Vue Apollo",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "graphql",
+				color: "pink-text-gradient",
+			},
+
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+		],
+		image: plan,
+		source_code_link: "",
+		web_link: "https://staging.uymp.org/",
+	},
+
+	{
+		name: "Tarixchi - Your Gateway to Unlock Uzbekistan's Hidden History",
+		description:
+			"Unique platform dedicated to unveiling the rich tapestry of Uzbekistan's history, one historically meaningful document at a time.",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Nuxt.js",
+				color: "green-text-gradient",
+			},
+			{
+				name: "Axios",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Headless",
+				color: "pink-text-gradient",
+			},
+
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+			{
+				name: ".Net",
+				color: "blue-text-gradient",
+			},
+		],
+		image: tarixchi,
+		source_code_link: "",
+		web_link: "https://tarixchi.vercel.app/",
+	},
+	{
+		name: "Split bore, buy product by splitting payments",
+		description:
+			"SplitBor.uz allows you to divide the cost of your purchase into four equal parts, free from any extra fees or overpayments. This feature simplifies payment processes, making shopping more accessible and budget-friendly",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Nuxt.js",
+				color: "green-text-gradient",
+			},
+			{
+				name: "Axios",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Headless",
+				color: "pink-text-gradient",
+			},
+
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+			{
+				name: ".Net",
+				color: "blue-text-gradient",
+			},
+		],
+		image: splitbor,
+		source_code_link: "",
+		web_link: "https://split-bor.vercel.app/",
+	},
+
+	{
+		name: "Delicious recipe",
+		description:
+			"Indulge in culinary exploration! Discover and share delectable recipes on this intuitive, user-friendly platform designed for passionate food enthusiasts. Join the flavorful journey!",
+		tags: [
+			{
+				name: "Vue",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "Hasura",
+				color: "green-text-gradient",
+			},
+			{
+				name: "graphql",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Go",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Pinia",
+				color: "blue-text-gradient",
+			},
+		],
+		image: recipe,
+		source_code_link: "https://github.com/yonaSisay/recipe_frontend",
+		web_link: "https://recipe-frontend-omega.vercel.app/",
+	},
+	{
+		name: "Shemach",
+		description:
+			"simple ecommerce platform that help users to make order of product and see the product they orderd, help them to search product and checkout",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "firebase",
+				color: "green-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Redux",
+				color: "blue-text-gradient",
+			},
+		],
+		image: shemach,
+		source_code_link: "https://github.com/yonaSisay/shemach",
+		web_link: "https://shemach-612da.web.app/",
+	},
+];
+
+export { services, technologies, experiences, projects };
